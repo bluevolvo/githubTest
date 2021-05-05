88832a04805203264971b6c0f07437c4fb7877f4
bluevolvo <72864811+bluevolvo@users.noreply.github.com>
2021-05-05 22:34:38 +0000

    Update Currency.dart
    Removed the data variable

--- lib/Logic/Currency.dart
+++ lib/Logic/Currency.dart
@@ -5,12 +5,12 @@ import 'dart:convert';
 
 
 class GetData{
-  var _data;
+
   Future httpRequest (String selectedCurrency) async{
     var url = Uri.parse("https://rest.coinapi.io/v1/exchangerate/BTC/$selectedCurrency?apikey=27D6E83A-A648-4669-901E-89BBB9A4747F#");
     Response response =await get(url);
-    _data =response.body;
-   return jsonDecode(_data);
+    
+   return jsonDecode(response.body);
 
 }
 }
